637cde3f25ae2dc57f7af1a69d2ddef40c59ac71
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2022-08-19 23:03:45 +0200

    [camera] Migrate the camera view module to Swift

---
+++ packages/expo-camera/ios/EXCamera/TakePictureOptions.swift
@@ -0,0 +1,20 @@
+// Copyright 2022-present 650 Industries. All rights reserved.
+
+import ExpoModulesCore
+
+struct TakePictureOptions: Record {
+  @Field
+  var id: Int = 0
+
+  @Field
+  var quality: Double = 0
+
+  @Field
+  var base64: Bool = false
+
+  @Field
+  var exif: Bool = false
+
+  @Field
+  var fastMode: Bool = false
+}
